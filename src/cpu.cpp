805e7176cc50653387f334068747121fb81c0c9a
Ian Wesley Campbell <ixc9335@cs.rit.edu>
2021-04-20 21:15:49 -0400

    Added initial setup of Standard hardware

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1,6 +1,36 @@
 #include "cpu.h"
 
 //=========================================<CPU Objects>==========================================//
+Bus ALU1Out("ALU1Out", kAddrBusSize);
+Bus ALU2Out("ALU2Out", kAddrBusSize);
+Bus ALU1A("ALU1A", kAddrBusSize);
+Bus ALU1B("ALU1B", kAddrBusSize);
+Bus ALU2A("ALU2A", kAddrBusSize);
+Bus ALU2B("ALU2B", kAddrBusSize);
+
+BusALU alu1("ALU1", kAddrBusSize);
+BusALU alu2("ALU2", kAddrBusSize);
+
+Counter pc("PC", kRegSize);
+Counter mdr("MDR", kRegSize);
+StorageObject ir("IR", kRegSize);
+Clearable r0("R0", kRegSize);
+Clearable r1("R1", kRegSize);
+Clearable r2("R2", kRegSize);
+Clearable r3("R3", kRegSize);
+Clearable r4("R4", kRegSize);
+Clearable r5("R5", kRegSize);
+Clearable r6("R6", kRegSize);
+Clearable r7("R7", kRegSize);
+Clearable r8("R8", kRegSize);
+Clearable r9("R9", kRegSize);
+Clearable rA("RA", kRegSize);
+Clearable rB("RB", kRegSize);
+Clearable pR("PR", kRegSize);
+Clearable pB("PB", kRegSize);
+Clearable pS("PS", kRegSize);
+
+Memory m("Main Memory", kAddrBusSize, kDataBusSize);
 
 //=========================================<uPU Objects>==========================================//
 
