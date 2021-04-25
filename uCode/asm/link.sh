02324a44a59a351c6936b0807d4f4be6f6333648
rmh2805 <rmh2805@g.rit.edu>
2021-04-24 22:20:10 -0400

    Added and linked ScaleOp

--- uCode/asm/link.sh
+++ uCode/asm/link.sh
@@ -1,7 +1,15 @@
 #! /bin/bash
 ./assemble.sh fetch.asm 0 > fetch.obj
-./assemble.sh GetOperand.asm 100 > GetOperand.obj
-./assemble.sh Decode.asm 200 > Decode.obj
-./assemble.sh EvalOp.asm 300 > EvalOp.obj
-./assemble.sh RegVal.asm A0 > RegVal.obj 
-python3 ../../Asm/uAsm/link.py uCode.obj 0 fetch.obj 0 RegVal.obj A0 GetOperand.obj 100 Decode.obj 200 EvalOp.obj 300 
+./assemble.sh GetOperand.asm    100 > GetOperand.obj
+./assemble.sh Decode.asm        200 > Decode.obj
+./assemble.sh EvalOp.asm        300 > EvalOp.obj
+./assemble.sh RegVal.asm         A0 > RegVal.obj 
+./assemble.sh ScaleOp.asm       1A0 > ScaleOp.obj
+
+python3 ../../Asm/uAsm/link.py uCode.obj 0 \
+        fetch.obj       0   \
+        RegVal.obj      A0  \
+        GetOperand.obj  100 \
+        ScaleOp.obj     1A0 \
+        Decode.obj      200 \
+        EvalOp.obj      300 
