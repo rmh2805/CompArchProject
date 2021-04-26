b15bdc926bfe47b45275041cf16f2f2f49fbff99
rmh2805 <rmh2805@g.rit.edu>
2021-04-26 19:40:31 -0400

    Implemented the other CTR instructions

--- uCode/asm/CTR.asm
+++ uCode/asm/CTR.asm
@@ -2,3 +2,11 @@
 
 # HLT
     0x00 sub pc i0 i1; goto Writeback       # Set up PC overflow
+# NOP
+    0x01 none; goto Writeback
+# CLR
+    0x02 mov uR0 i0; goto Writeback
+# DMP
+    0x03 none; goto Writeback
+# OUT
+    0x04 mov o1 OP1Val; goto Writeback
