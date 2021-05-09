b4559731ef685dfb9f9532a8e7c4f8a572ebc854
Ian Wesley Campbell <ixc9335@cs.rit.edu>
2021-05-08 21:04:57 -0400

    Finished fib! Currently does the 10th fib number which is 0x37 in hex or 55 in Decimal"

--- src/fib.asm
+++ src/fib.asm
@@ -1,23 +1,22 @@
 .data 
-    a0 = .word 5
 
 .code
 start:
     # We will use r3 as the return reg
-    mov %pS, $0xFFFFF
-    mov %rA, $7
+    mov %r9, $0xFFFFF
+    mov %rA, $10
     jal fib
     dmp %r3
     hlt
 
 fib:
     # push rA and 2 regs on the stack
-    sub %pS, %pS, $0x14
-    mov 0(%pS), %pR
-    mov 4(%pS), %r0
-    mov 8(%pS), %r1
-    mov 12(%pS), %r2
-    mov 16(%pS), %rA
+    sub %r9, %r9, $0x14
+    mov 0(%r9), %pR
+    mov 4(%r9), %r0
+    mov 8(%r9), %r1
+    mov 12(%r9), %r2
+    mov 16(%r9), %rA
     
     # Base Case
     mov %r3, %rA
@@ -26,18 +25,18 @@ fib:
     # Revursive calls
     sub %rA, %rA, $1
     jal fib
-    add %r1, %r3, $0 
+    mov %r1, %r3
     sub %rA, %rA, $1
     jal fib
     add %r0, %r1, %r3
     mov %r3, %r0
 
 fibRet:
-    mov %pR, 0(%pS) 
-    mov %r0, 4(%pS) 
-    mov %r1, 8(%pS) 
-    mov %r2, 12(%pS) 
-    mov %rA, 16(%pS) 
-    add %pS, %pS, $0x14
+    mov %pR, 0(%r9) 
+    mov %r0, 4(%r9) 
+    mov %r1, 8(%r9) 
+    mov %r2, 12(%r9) 
+    mov %rA, 16(%r9) 
+    add %r9, %r9, $0x14
     ret
 
