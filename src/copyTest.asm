c53b1daaf95ae6c62779e95d66bf7e762abb4f9d
rmh2805 <rmh2805@g.rit.edu>
2021-05-11 10:30:27 -0400

    Chaned copyTest to demo operand interp

--- src/copyTest.asm
+++ src/copyTest.asm
@@ -1,28 +1,63 @@
 .data
-
+    bnk = .byte 0x00 0x00 0x00 0x00 0x00 0x00
+    src = .byte 0x01 0x02 0x03 0x02 0x01 0x00
+    dst = .byte 0x00 0x00 0x00 0x00 0x00 0x00
+    
 .code
 start:
+    mov %r0, $0
+    dmp dst(%r0)
+    add dst, $4
+    dmp dst(%r0)
+    add dst, $4
+    dmp dst(%r0)
+    add dst, $4
+    dmp dst(%r0)
+    add dst, $4
     
-    mov %r6, $0xFF
-    mov %r5, $4
-    mov %r4, $0
-
-push_str:
-    bge %r4, %r5, push_str_done
-    mul %r3, %r4, $4
-    add %r0, %r3, %r6
-    mov (%r0), $0xFFFFFFFF 
-    add %r4, %r4, $1
-    jmp push_str
-
-push_str_done:
-    dmp 16(%r6)
-    dmp 20(%r6)
-    dmp 24(%r6)
-    dmp 28(%r6)
-    cpy 0xFF, 16(%r6), $0, $16
-    dmp 16(%r6)
-    dmp 20(%r6)
-    dmp 24(%r6)
-    dmp 28(%r6)
+    nop
+    nop
+    nop
+    cpy src, dst
+    
+    mov %r0, $0
+    dmp dst(%r0)
+    add %r0, $4
+    dmp dst(%r0)
+    add %r0, $4
+    dmp dst(%r0)
+    add %r0, $4
+    dmp dst(%r0)
+    add %r0, $4
+    
+    nop
+    nop
+    nop
+    cpy bnk, dst, $4
+    
+    mov %r0, $0
+    dmp dst(%r0)
+    add %r0, $4
+    dmp dst(%r0)
+    add %r0, $4
+    dmp dst(%r0)
+    add %r0, $4
+    dmp dst(%r0)
+    add %r0, $4
+    
+    nop
+    nop
+    nop
+    cpy src, dst, $0, $3
+    
+    mov %r0, $0
+    dmp dst(%r0)
+    add %r0, $4
+    dmp dst(%r0)
+    add %r0, $4
+    dmp dst(%r0)
+    add %r0, $4
+    dmp dst(%r0)
+    add %r0, $4
+    
     hlt
